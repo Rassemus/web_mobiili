c6eb3b6202e14d0e9c95f591f907a6c517bf4b9c
Rassemus <rasse91@gmail.com>
2026-02-21 10:29:02 +0200

    project addition

---
+++ projekti/js/script.js
@@ -0,0 +1,92 @@
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+});
+
+
+const buttons = document.querySelectorAll(".read-more-btn");
+
+if(buttons) {
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const card = button.closest(".service-card");
+            card.classList.toggle("expanded");
+
+            button.textContent =
+                card.classList.contains("expanded")
+                    ? "Näytä vähemmän"
+                    : "Lue lisää";
+        });
+    });
+}
+
+
+// Referenssit 
+const references = [
+    {
+        id: 1,
+        name: "FinTech Oy",
+        description: "Toteutimme kokonaisvaltaisen riskikartoituksen ja rakensimme yritykselle digitaalisen riskienhallintamallin.",
+        quote: "Yhteistyö toi selkeyttä riskienhallintaan ja paransi raportointia merkittävästi."
+    },
+    {
+        id: 2,
+        name: "Teollisuus Group",
+        description: "Sparrasimme johtoa strategisten riskien tunnistamisessa ja rakensimme jatkuvan riskiseurantamallin.",
+        quote: "Ammattilaisten tuki oli ratkaisevaa päätöksenteon kehittämisessä."
+    },
+    {
+        id: 3,
+        name: "Maailman vakuutajat Oy",
+        description: "Toteutimme hybridinä, tarvittava määrä konsultointia sekä kokonaisvaltainen alustan käyttöoikeus.",
+        quote: "Riskeeraajien hybridi toteutus soveltui hyvin yrityksellemme. Saimme tarvittavan määrän konsultaatiota kun oma osaamisemme ei riittänyt vaikka riskien hallinta alusta oli todella helppo käyttöinen."
+    }
+];
+
+const container = document.getElementById("references-container");
+
+if(container){
+    references.map(ref => {
+        const card = document.createElement("div");
+        card.classList.add("reference-card");
+
+        card.innerHTML = `
+            <div id="${ref.id}">
+            <span>${ref.name}</span>
+            </div>
+            <blockquote>"${ref.quote}"</blockquote>
+            <p>${ref.description}</p>
+        `;
+
+        container.appendChild(card);
+    });
+}
+
+
+// CONTACT 
+
+const form = document.getElementById("contactForm");
+
+if(form){
+    const originalForm = form.innerHTML;
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault(); // Estää sivun reloadin
+        
+        form.innerHTML = `
+        <div class="success-box">
+        <h3>Kiitos yhteydenotostasi!</h3>
+        <p>Palaamme asiaan mahdollisimman pian.</p>
+        </div>
+        `;
+
+        // Palauta lomake 6 sekunnin kuluttua
+        setTimeout(() => {
+            form.innerHTML = originalForm;
+        }, 6000);
+    });
+}
+        
